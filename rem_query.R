e1c2b00e1333774a131c46b7b8a48c75ff5aed5f
Huli <huli@eitschei.com>
2017-01-13 15:38:59 +0100

    Query changed.

--- rem_query.R
+++ rem_query.R
@@ -8,36 +8,51 @@ install.packages("ggplot2")
 library(RODBC)
 library(data.table)
 library(ggplot2)
+library(dplyr)
 
 connection <- odbcDriverConnect('driver={SQL Server};server=sqlrem\\sql2005;database=rem_unit_test;trusted_connection=true')
-person_table <- sqlQuery(connection, 'select OID, PersonID, Name, Vorname from dbo.Person')
-# adresse_table <- sqlQuery(connection, 'select OID, Strasse, Ort, Plz from Adresse')
-# adressezuordnung_table <- sqlQuery(connection, 'select * from dbo.Adresszuordnung')
-# 
-# newest_adresszuordnungen <- as.data.table(adressezuordnung_table)[
-#               , .(GueltigAb, PersonOID, AdresseOID)][ 
-#                 , GueltigAb := max(GueltigAb), by=PersonOID]
+mietzinse <- sqlQuery(connection, "select mz.GueltigAb ,mz.BetragNetto, abr.KantonCD, 
+		                  abr.Ort,
+                      abr.Bezeichnung1, 
+                      art.Bez,
+                      art.ObjektKategorieCD,
+                      o.AnzahlZimmerCD
+                      from Mietzins mz
+                      join Objekt o on mz.ObjektOID = o.OID
+                      join Haus h on o.HausOID = h.OID
+                      join AbrechEinheit abr on abr.OID = h.AbrechEinheitOID
+                      join ObjektArt art on art.OID = o.ObjektArtOID
+                      where GueltigAb > \'2000-01-01\'
+                      and GueltigAb < \'2017-01-01\'
+                      order by Ort")
+
+mietzinse %>% 
+  group_by(GueltigAb < '2002-01-01') %>% 
+  summarise(avg_betrag = mean(BetragNetto))
+
+mietzinse %>% 
+  mutate(year = format(GueltigAb, "%Y")) %>% 
+  mutate(month = format(GueltigAb, "%m"))
+  group_by(year_month) %>% 
+  summarise(avg_betrag = mean(BetragNetto))
+
+mietzinse[1:10,] %>% 
+  mutate(year_month = cat(year(GueltigAb), months(GueltigAb), sep = ""))
+
+summary(mietzinse)
+str(mietzinse)
+
+cat("adsfad", 4)
+
+mietzinse[1, "GueltigAb"] %>%  format("%m")  
+mietzinse[1, "GueltigAb"] %>%  format("%Y")  
+
+mietzinse %>% 
+  filter(!is.na(AnzahlZimmerCD) & AnzahlZimmerCD == "3.5") %>% 
+  ggplot(aes(x = GueltigAb, y = BetragNetto)) +
+  geom_point()
 
 
-personen <- as.data.table(person_table)
-personen <- personen[ Name != " "][!is.na(Name)]
-
-personen[, Vorkommen := .N, by = Name]
-
-nrow(personen)
-setkey(personen, Name)
-key(personen)
-
-str(personen)
-unique_personen <- personen[, .(Name, max(Vorkommen)), by=Name]
-str(unique_personen)
-
-ordered_vorkommen <- unique_personen[order(-V2)][1:10]
-
-ggplot(ordered_vorkommen[order(-V2)]) +
-  aes(y = V2, x = Name) +
-  geom_point()  +
-  ylim(0, 1000)
 
 
 close(connection)
